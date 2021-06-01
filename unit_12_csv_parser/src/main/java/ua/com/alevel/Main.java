4aa9795dc4cc00d740143356c0afaa0f673f1535
Yaroslav <yaroslavsid05@gmail.ru>
2021-06-01 19:59:43 +0300

    change hard code (part 2)

--- unit_12_csv_parser/src/main/java/ua/com/alevel/Main.java
+++ unit_12_csv_parser/src/main/java/ua/com/alevel/Main.java
@@ -8,10 +8,11 @@ import ua.com.alevel.table.Table;
 import java.util.List;
 
 public class Main {
+    public static final String path = "cities.csv";
 
     public static void main(String[] args) {
         CSVParser parser = new CSVParser();
-        Table table = new Table(parser.getTable());
+        Table table = new Table(parser.getTable(path));
         List<Cities> citiesList = new PropertyBind().initializeProperties(Cities.class, table);
         for (Cities cities : citiesList) {
             System.out.println(cities);
--- unit_12_csv_parser/src/main/java/ua/com/alevel/parser/CSVParser.java
+++ unit_12_csv_parser/src/main/java/ua/com/alevel/parser/CSVParser.java
@@ -6,17 +6,18 @@ import com.opencsv.exceptions.CsvValidationException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CSVParser {
-    public String[][] getTable(){
+    public String[][] getTable(String path){
         String[] row;
-        LinkedList<String[]> rows = new LinkedList<>();
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream("cities.csv")) {
+        List<String[]> rows = new ArrayList<>();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(path)) {
             try (CSVReader reader = new CSVReader(new InputStreamReader(Objects.requireNonNull(input)))) {
                 while ((row = reader.readNext()) != null) {
-                    rows.addLast(row);
+                    rows.add(row);
                 }
             }
         } catch (IOException | CsvValidationException e) {
