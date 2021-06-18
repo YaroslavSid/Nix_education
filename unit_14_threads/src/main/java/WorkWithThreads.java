f65caf86352c3131552c4bdc090f94c6c3cb58fe
Yaroslav <yaroslavsid05@gmail.ru>
2021-06-18 12:31:07 +0300

    change collection

--- unit_14_threads/src/main/java/WorkWithThreads.java
+++ unit_14_threads/src/main/java/WorkWithThreads.java
@@ -1,81 +1,71 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class WorkWithThreads {
     static String path = "output.txt";
-    static final List<String> value = Collections.synchronizedList(new ArrayList<>());
+    static final BlockingQueue<String> value = new ArrayBlockingQueue<>(16);
 
     public static void main(String[] args) {
-        new InputFromConsole().start();
-        new OutputToFile(path).start();
+        new Producer().start();
+        new Consumer(path).start();
     }
 
 
-
-    static class InputFromConsole extends Thread {
+    static class Producer extends Thread {
         @Override
         public void run() {
             System.out.println("Enter something(exit = quit):");
             Scanner scanner = new Scanner(System.in);
             while (true) {
-                synchronized (value) {
-                    value.add(scanner.nextLine());
-                    value.notify();
-                    if (value.get(0).equals("quit")) {
-                        System.out.println("Exit from system");
-                        break;
-                    }
-                }
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                value.add(scanner.nextLine());
+                if (value.contains("quit")) {
+                    System.out.println("Exit from system");
+                    break;
                 }
             }
         }
     }
 
 
-
-    static class OutputToFile extends Thread {
+    static class Consumer extends Thread {
         String path;
         String flag = "";
+        String input;
 
-        OutputToFile(String path) {
+        Consumer(String path) {
             this.path = path;
         }
 
         @Override
         public void run() {
             while (true) {
-                synchronized (value) {
-                    try {
-                        value.wait();
-                    } catch (InterruptedException e) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    input = value.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (input.equals("quit")) {
+                    break;
+                } else if (!flag.equals(input)) {
+                    try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path))) {
+                        bufferedWriter.write(input);
+                        flag = input;
+                    } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                    if (value.get(0).equals("quit")) {
-                        break;
-                    } else if (!flag.equals(value.get(0))) {
-                        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path))) {
-                            bufferedWriter.write(value.get(0));
-                            flag = value.get(0);
-                            value.remove(0);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    } else {
-                        System.out.println("repeat, value don't write to file");
-                        value.remove(0);
-                    }
+                } else {
+                    System.out.println("repeat, value don't write to file");
                 }
             }
         }
     }
-
 }
 
 
